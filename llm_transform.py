ef5f96c1b736f4cd60f98f9208ee7cc8e58e542a
neudachina <eva.neudachina@gmail.com>
2025-05-07 14:03:52 +0300

    Update prompt for synonyms

--- llm_transform.py
+++ llm_transform.py
@@ -7,27 +7,40 @@ if torch.backends.mps.is_available():
     torch.mps.empty_cache()
 
 # Load Qwen model
-model_id = "Qwen/Qwen2.5-0.5B"
+model_id = "Qwen/Qwen2.5-7B"
 # model_id = "Qwen/Qwen3-0.6B"
 tokenizer = AutoTokenizer.from_pretrained(model_id, trust_remote_code=True)
 model = AutoModelForCausalLM.from_pretrained(model_id, trust_remote_code=True)
 # llm = pipeline("text-generation", model=model, tokenizer=tokenizer, max_new_tokens=10)
-llm = pipeline("text-generation", model=model, tokenizer=tokenizer, max_new_tokens=64, device=-1)  # use CPU
+# llm = pipeline("text-generation", model=model, tokenizer=tokenizer, max_new_tokens=64, device=-1)  # use CPU
 # llm = pipeline("text-generation", model=model, tokenizer=tokenizer, device=0 if torch.backends.mps.is_available() else -1)
 
+
+if torch.cuda.device_count() > 1:
+    print("Using", torch.cuda.device_count(), "GPUs")
+    model = torch.nn.DataParallel(model)
+
+wrapped_model = model
+if isinstance(model, torch.nn.DataParallel):
+    wrapped_model = model.module
+
+llm = pipeline("text-generation", model=wrapped_model, tokenizer=tokenizer, max_new_tokens=100, device=0)
+
+
+
 def transform_prompt(prompt: str, instruction: str) -> str:
-    full_prompt = f"""You are a helpful AI that modifies image generation prompts.
-                        Instruction: {instruction}
-                        Original prompt: "{prompt}"
-                        Transformed prompt: <your response>"""
+    full_prompt = f"""You are a helpful AI that modifies image generation prompts. 
+\nInstruction: {instruction}
+\nOriginal prompt: {prompt}
+Transformed prompt:"""
 
     print("\n=== Prompt Sent to LLM ===")
     print(full_prompt)
 
     response = llm(full_prompt)[0]['generated_text']
 
-    print("\n=== Raw LLM Output ===")
-    print(response)
+    # print("\n=== Raw LLM Output ===")
+    # print(response)
 
     # Extract only the transformed part after the delimiter
     if "Transformed prompt:" in response:
@@ -41,14 +54,18 @@ def transform_prompt(prompt: str, instruction: str) -> str:
 
     return transformed
 
-def run_batch_transform(input_csv="data/prompts_cut.csv", output_csv="data/transformed_prompts_cut.csv"):
+def run_batch_transform(input_csv="data/prompts.csv", output_csv="data/transformed_prompts_cut.csv"):
     df = pd.read_csv(input_csv)
 
     instructions = {
-        "synonyms": "Replace some content words in the prompt with their SYNONYMS (words with the same meaning but different spelling). "
-        "Keep the same amount of words in transformed phrase. "
-        "EXCLUDE explanations."
-        "ELAMPLE: `A cat on a mat` -> `A feline resting on a rug`",
+        "synonyms": "Replace some content words in the original prompt with their SYNONYMS i.e. words with the same meaning but different spelling. "
+        "Keep the same amount of words in transformed prompt. "
+        "EXCLUDE all the explanations - return only the transformed original prompt. "
+        "\nELAMPLE: "
+        "\nOriginal prompt: A cat on a mat "
+        "\nTransformed prompt: A feline resting on a rug "
+        "\nNOW YOUR RESULT: ",
+        
         # "neutral_add": "Add polite or neutral quality-enhancing phrases (e.g. 'high quality', 'please', etc.) to the prompt.",
         # "word_reorder": "Slightly reorder the words in the prompt while keeping the meaning the same.",
     }
